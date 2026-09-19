19c2431299fb1bb10def955e4dfa81d97621b5be
VaibhavPatel0421 <raunakp9621@gmail.com>
2026-09-19 14:46:31 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,12 @@
+# Verma Medical Store - Medicine Class
+
 class Medicine:
     def __init__(self, name, price, quantity):
         self.name = name
         self.price = price
         self.quantity = quantity
+
+    def __str__(self):
+        return f"{self.name:<20} | Price: Rs.{self.price:<8.2f} | Stock: {self.quantity}"
+
+
